cd883c0e8dc79ef1d4c8919f67d75a33190b7d70
Gaspard Beernaert <gaspard@gmail.com>
2020-12-09 09:42:15 +0100

    some internal promotion :-)

--- init.sh
+++ init.sh
@@ -3,4 +3,6 @@ git init
 npm i
 grep -v "init" package.json > tmpfile && mv tmpfile package.json
 rm -r .github
+git add .
+git commit -m "chore(first commit): template from https://github.com/Lissandre/three_template"
 rm ./init.sh
