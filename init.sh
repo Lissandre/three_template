0cc2f28cabae09d0fcc7db73504d191de3a884dc
Lissandre <lissandre.pasdeloup@hetic.net>
2020-12-10 22:28:19 +0100

    organize script

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 rm -rf .git
-git init
-npm i
-grep -v "init" package.json > tmpfile && mv tmpfile package.json
 rm -r .github
+npm i
+git init
 git add .
 git commit -m "chore(first commit): template from https://github.com/Lissandre/three_template"
+grep -v "init" package.json > tmpfile && mv tmpfile package.json
 rm ./init.sh
